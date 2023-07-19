dd2dff08b011357c18f15e5d53d939fa42fc7f7c
Abdisa <abdisadev@gmail.com>
2023-07-19 23:57:17 +0300

    signin with googlepopup and auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,3 +22,6 @@ const firebaseApp = initializeApp(firebaseConfig)
 const provider = new GoogleAuthProvider()
 
 provider.setCustomParameters({prompt: 'select_account'})
+
+export const auth = getAuth()
+export const signInWithgooglePopup = () => signInWithPopup(auth, provider)

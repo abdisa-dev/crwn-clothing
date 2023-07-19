a3ab85881b3d56a62c87b13f835c543681a5ea2e
Abdisa <abdisadev@gmail.com>
2023-07-20 00:11:32 +0300

    signin with google popup

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,15 @@
+import {signInWithgooglePopup} from "../../utils/firebase/firebase.utils"
+
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithgooglePopup();
+    console.log(response)
+  }
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign IN with google popup</button>
     </div>
   )
 }
